adc8da52dd2c8701697b54c7b57da16b1e075ca5
kallip <kallinpettit@gmail.com>
2018-07-26 20:44:05 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -12,7 +12,7 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 def perfect_10_recipe 
-perfect_10_recipe(perfect_10_ingredients) = {
+perfect_10_recipe = {
   :almond_flour => "2 cups",
   :gluten_free_whole_oats => "1 cup",
   :kosher_salt => "1/2 tea spoon",
