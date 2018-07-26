27610e7dcc95372e91869d6583df48dd23120ce4
kallip <kallinpettit@gmail.com>
2018-07-26 20:31:23 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -21,10 +21,9 @@ perfect_10_recipe {
   :baking_powder => "1/2 tea spoon"
   :baking_soda => "1/4 tea spoon"
   :xanthan_Gum => "1/2 tea spoon"
-  :slivered_almonds_lightly_toasted => "1/4 cup"
+  :slivered_almonds => "1/4 cup"
   :mini_dark_chocolate_chips => "3/4 cup"
   :olive_oil => "1/2 cup"
-  :agave => "1/4 cup"
 }
 end
 #2. Write a method that simply returns (not puts) your perfect_10 hash
